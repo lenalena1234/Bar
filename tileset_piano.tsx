d848f60cba2ec0b3a1faeacfacf516738f37027c
lenalena1234 <82158764+lenalena1234@users.noreply.github.com>
2021-04-09 14:02:26 +0200

    Delete tileset_piano.tsx

--- tileset_piano.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="tileset_piano" tilewidth="45" tileheight="45" tilecount="289" columns="17">
- <image source="tileset_piano.png" trans="feffff" width="768" height="768"/>
-</tileset>
