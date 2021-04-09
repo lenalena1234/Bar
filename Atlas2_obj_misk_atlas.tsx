825300ffa381c9cbdc874d97c80be6dba4e718dc
lenalena1234 <82158764+lenalena1234@users.noreply.github.com>
2021-04-09 14:29:05 +0200

    Add files via upload

---
+++ Atlas2_obj_misk_atlas.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Atlas2_obj_misk_atlas" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="Atlas2_obj_misk_atlas.png" trans="feffff" width="1024" height="1024"/>
+</tileset>
---
+++ FOOD_new.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="FOOD_new" tilewidth="32" tileheight="32" tilecount="525" columns="21">
+ <image source="FOOD.png" trans="feffff" width="702" height="807"/>
+</tileset>
